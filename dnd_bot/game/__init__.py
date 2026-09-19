4c5b59ac90f05ee5bdcb64697248ac088cd7d847
chronuscontabilidade-star <chronuscontabilidade@gmail.com>
2026-09-19 00:56:46 -0300

    feat: add deterministic D&D core engine

---
+++ dnd_bot/game/__init__.py
@@ -0,0 +1,27 @@
+"""Núcleo determinístico das regras do jogo.
+
+A IA pode interpretar e narrar ações, mas as regras mecânicas devem ser
+resolvidas por este pacote.
+"""
+
+from .character import Character
+from .dice import DiceRoll, roll, roll_d20
+from .engine import GameEngine
+from .rules import (
+    ability_modifier,
+    ability_check,
+    saving_throw,
+    proficiency_bonus,
+)
+
+__all__ = [
+    "Character",
+    "DiceRoll",
+    "GameEngine",
+    "ability_check",
+    "ability_modifier",
+    "proficiency_bonus",
+    "roll",
+    "roll_d20",
+    "saving_throw",
+]
